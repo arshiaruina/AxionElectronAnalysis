3369d7d34c1f6f438a55424363438ebf06187369
arshiaruina <ruina.a19@gmail.com>
2019-01-30 19:16:36 +0100

    obtained table numbers but error in parsing and storing the correct table

--- src/readFile.cc
+++ src/readFile.cc
@@ -1,81 +1,118 @@
+//Code to read the opacity files and extract the required tables from them to be accessed later
+//Author: Arshia Ruina
+
 #include <iostream>
 #include <iomanip>
 #include <fstream>
+#include <vector>
 
-int main() {
-	
-	
-	std::string filename = "../resources/opacity_tables/OP17.1.0-0.0-0.0-0.0-0.0-0.0-0.0-0.0-0.0-0.0-0.0-0.0-0.0-0.0-0.0.stored";
+struct SELECT {
+	std::string H_massFrac = "";
+	std::string He_massFrac = "";
+	std::string tableIndex = "";
+	int lineNumber = 0;
+};
+
+int main() { //separate main from other functions	
+
+	//TODO: filename has to be eventually chosen according to the [X_Z]	
+	std::string in_filename = "../resources/opacity_tables/OP17.1.0-0.0-0.0-0.0-0.0-0.0-0.0-0.0-0.0-0.0-0.0-0.0-0.0-0.0-0.0.stored";
 	std::string line;
-	std::ifstream file;
+	std::ifstream in_file;
 
-	file.open(filename.c_str());
+	int lineNumber = 0;
+	int posX = 0;
+	int posY = 0;
+	int posTableIndex = 0;
+	//TODO: the number of tables that have to extracted
+	//this number would depend on the required combinations of H and He 
+	//mass fractions for a particular set of [X_Z] 
+	std::vector<SELECT> select;
+	std::vector<std::string> H_massFrac_list = {"0.9980","0.9999"};	
+	std::vector<std::string> He_massFrac_list = {"0.0000","0.0000"};	
+	int input = H_massFrac_list.size();//or size of He_massFrac_list
+	std::cout << "size of list: " << input << std::endl;
 
-	if(!file.good()){
+	for(int i = 0; i < input; i++) {
+		select.push_back(SELECT());
+		select.back().H_massFrac = H_massFrac_list[i];
+		select.back().He_massFrac = He_massFrac_list[i];
+	}
+	
+	in_file.open(in_filename.c_str());
+
+	if(!in_file.good()){
         	std::cout << "Problem with file!" << std::endl;
 		return 1;
 	}
 
-	int lineNumber = 0;
-
-	while(!file.eof()){
-		std::getline(file, line);
+	while(!in_file.eof()){
+		std::getline(in_file, line);
 		++lineNumber;
-		if(lineNumber < 63 || lineNumber > 188) continue;
-		
-		//if(line.empty()) continue;
-		//if(line[0] != '*') continue;
-		//std::istringstream iss_line(line);
-		std::string tableIndex = line.substr(7,3);
-		int posX = line.find("X");
-		int posY = line.find("Y");
-		std::string H_massFrac = line.substr(posX+2,6);
-		std::string He_massFrac = line.substr(posX+2,6);
-		if(H_massFrac == "0.9980" && He_massFrac == "0.0000")
-			std::cout << "Table Index: " << tableIndex << std::endl;
+
+		if(lineNumber > 63 && lineNumber < 189){		
+			for(int i = 0; i < input; i++){
+				//std::istringstream iss_line(line);
+				std::string tableIndex;
+				posX = line.find("X");
+				posY = line.find("Y");
+				std::string H_massFrac = line.substr(posX+2,6);
+				std::string He_massFrac = line.substr(posY+2,6);
+				if(H_massFrac == select[i].H_massFrac && He_massFrac == select[i].He_massFrac){ 
+					tableIndex = line.substr(8,3);
+					select[i].tableIndex = tableIndex;
+					std::cout << "Selected mass fraction of H: " << select[i].H_massFrac << std::endl;
+					std::cout << "Selected mass fraction of He: " << select[i].He_massFrac << std::endl;
+					std::cout << "Selected table: " << select[i].tableIndex << std::endl;
+				}	
+			}
+		}
+
+		if(lineNumber > 240 ){	
+			for(int i = 0; i < input; i++){
+				std::string tableIndex; 
+				std::size_t posTableIndex = line.find("TABLE");
+				if (posTableIndex!=std::string::npos){
+					tableIndex = line.substr(posTableIndex+7,3);
+					std::cout << "Table number: " << tableIndex << std::endl;
+				}
+				else continue;
+				if(tableIndex == select[i].tableIndex){
+					select[i].lineNumber = lineNumber;
+					std::cout << "Line number for start of table: " << lineNumber << std::endl;
+				}
+			}
+		}
 	}
+
+	while(!in_file.eof()){
+		for(int i = 0; i < input; i++){
+
+			std::cout << "DEBUG: entered the loop to run over input file again and write the output file" << std::endl;
+ 
+			std::stringstream s;
+			std::ofstream out_file;
+			std::string out_filename = "../resources/extracted_tables/" + select[i].H_massFrac + "-" + select[i].He_massFrac + "-" + in_filename.substr(32,60) + ".stored";
+			out_file.open(out_filename);
+	
+			std::getline(in_file, line);
+			++lineNumber;
+
+			int line_counter = 0;
+			if(lineNumber == select[i].lineNumber){
+				std::cout << "Found line..." << std::endl;
+				while(line_counter < 76){
+					s << line << std::endl;
+					++line_counter;
+				}
+			}
+			std::cout << "Parsed table " << select[i].tableIndex << std::endl;
+			out_file << s.str() << std::endl;
+			out_file.close();
+			std::cout << "Output file closed" << std::endl;
+		}
+	}
+	in_file.close();
+	std::cout << "END" << std::endl;
 }
 
-//
-//        // if not a comment, line should be added
-//        // before we can add intensity, we need to include the conversion
-//        // probability from axions to photons
-//        iss_line >> _energy >> _axionIntensity;
-//while (inFile >> x) {
-//        sum = sum + x;
-//    }
-//
-//    inFile.close();
-//    cout << "Sum = " << sum << endl;
-//    return 0;
-//}
-//
-////--------------------------------------------------//
-//
-//// get ifstream to file which contains spectrum
-//std::ifstream axionSpecIfStream;
-//axionSpecIfStream.open(axionSpectrumPath.c_str(), std::ifstream::in);
-//if(axionSpecIfStream.good() == false){
-//    std::cout << "WARNING: Axion spectrum not found. May result in unexpected behaviour!"
-//              << std::endl;
-//}
-//// and create empty branches for energy and intensity
-//// treeAxionSpectrum->Branch("energy",    &_energy, 1000000);
-//// treeAxionSpectrum->Branch("intensity", &_axionIntensity, 1000000);
-//// now loop over file and append lines to tree branches
-//
-//while(axionSpecIfStream.good() &&
-//      !axionSpecIfStream.eof()){
-//
-//    // as long as still something to read...
-//    std::string line;
-//    std::getline(axionSpecIfStream, line);
-//    std::istringstream iss_line(line);
-//    if(line[0] != '#'){
-//        // if not a comment, line should be added
-//        // before we can add intensity, we need to include the conversion
-//        // probability from axions to photons
-//        iss_line >> _energy >> _axionIntensity;
-//
-//        //filling myfluxhisto
-//        myfluxhisto->Fill(_energy,_axionIntensity);
