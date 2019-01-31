a98a26100cc8e3216a34e4d737de4b85f76262c0
arshiaruina <ruina.a19@gmail.com>
2019-01-31 19:04:49 +0100

    successfully parsed opacity file, extracted and stored required table in an output file

--- src/readFile.cc
+++ src/readFile.cc
@@ -5,32 +5,22 @@
 #include <iomanip>
 #include <fstream>
 #include <vector>
+#include "readFile.h"
 
-struct SELECT {
-	std::string H_massFrac = "";
-	std::string He_massFrac = "";
-	std::string tableIndex = "";
-	int lineNumber = 0;
-};
+int main(){
+    readFile f;
+    f.GetTableIndex();
+    f.ReadAndStoreTable();
+    return 0;
+}
 
-int main() { //separate main from other functions	
-
-	//TODO: filename has to be eventually chosen according to the [X_Z]	
-	std::string in_filename = "../resources/opacity_tables/OP17.1.0-0.0-0.0-0.0-0.0-0.0-0.0-0.0-0.0-0.0-0.0-0.0-0.0-0.0-0.0.stored";
-	std::string line;
-	std::ifstream in_file;
+int readFile::GetTableIndex() {
 
 	int lineNumber = 0;
 	int posX = 0;
 	int posY = 0;
 	int posTableIndex = 0;
-	//TODO: the number of tables that have to extracted
-	//this number would depend on the required combinations of H and He 
-	//mass fractions for a particular set of [X_Z] 
-	std::vector<SELECT> select;
-	std::vector<std::string> H_massFrac_list = {"0.9980","0.9999"};	
-	std::vector<std::string> He_massFrac_list = {"0.0000","0.0000"};	
-	int input = H_massFrac_list.size();//or size of He_massFrac_list
+
 	std::cout << "size of list: " << input << std::endl;
 
 	for(int i = 0; i < input; i++) {
@@ -48,7 +38,7 @@ int main() { //separate main from other functions
 
 	while(!in_file.eof()){
 		std::getline(in_file, line);
-		++lineNumber;
+                ++lineNumber;
 
 		if(lineNumber > 63 && lineNumber < 189){		
 			for(int i = 0; i < input; i++){
@@ -60,59 +50,81 @@ int main() { //separate main from other functions
 				std::string He_massFrac = line.substr(posY+2,6);
 				if(H_massFrac == select[i].H_massFrac && He_massFrac == select[i].He_massFrac){ 
 					tableIndex = line.substr(8,3);
-					select[i].tableIndex = tableIndex;
+                                        select[i].tableIndex = tableIndex;
 					std::cout << "Selected mass fraction of H: " << select[i].H_massFrac << std::endl;
 					std::cout << "Selected mass fraction of He: " << select[i].He_massFrac << std::endl;
 					std::cout << "Selected table: " << select[i].tableIndex << std::endl;
 				}	
 			}
 		}
-
-		if(lineNumber > 240 ){	
-			for(int i = 0; i < input; i++){
-				std::string tableIndex; 
-				std::size_t posTableIndex = line.find("TABLE");
-				if (posTableIndex!=std::string::npos){
-					tableIndex = line.substr(posTableIndex+7,3);
-					std::cout << "Table number: " << tableIndex << std::endl;
-				}
-				else continue;
-				if(tableIndex == select[i].tableIndex){
-					select[i].lineNumber = lineNumber;
-					std::cout << "Line number for start of table: " << lineNumber << std::endl;
-				}
-			}
-		}
 	}
+        in_file.close();
+}		
 
-	while(!in_file.eof()){
-		for(int i = 0; i < input; i++){
+int readFile::ReadAndStoreTable() {
 
-			std::cout << "DEBUG: entered the loop to run over input file again and write the output file" << std::endl;
- 
-			std::stringstream s;
-			std::ofstream out_file;
-			std::string out_filename = "../resources/extracted_tables/" + select[i].H_massFrac + "-" + select[i].He_massFrac + "-" + in_filename.substr(32,60) + ".stored";
-			out_file.open(out_filename);
+        for(int i = 0; i < input; i++){
+
+        	int lineNumber = 0;
+        	int posTableIndex = 0;
+
+        	in_file.open(in_filename.c_str());
 	
-			std::getline(in_file, line);
-			++lineNumber;
+	        if(!in_file.good()){
+	                std::cout << "Problem with file!" << std::endl;
+	                return 1;
+	        }
 
-			int line_counter = 0;
-			if(lineNumber == select[i].lineNumber){
-				std::cout << "Found line..." << std::endl;
-				while(line_counter < 76){
-					s << line << std::endl;
-					++line_counter;
+                std::stringstream s;
+                std::ofstream out_file;
+                std::string out_filename = "../resources/extracted_tables/" + select[i].H_massFrac + "-" + select[i].He_massFrac + "-" + in_filename.substr(32,60) + ".stored";
+                out_file.open(out_filename);
+
+                while(!in_file.eof()){
+                        std::getline(in_file, line);
+                        ++lineNumber;
+                        if(lineNumber > 240 ){
+
+                                std::string tableIndex;
+                                std::size_t posTableIndex = line.find("TABLE");
+
+                                if (posTableIndex!=std::string::npos){ // 1. if at the first line of a table
+					//std::cout << "DEBUG: at first line of a table " << line << std::endl;
+				        tableIndex = line.substr(posTableIndex+7,3);
+                                        if(tableIndex == select[i].tableIndex){	// 1.a) if at the start of a table we want to access
+                                                //std::cout << "DEBUG: at first line of a table we want to access" << std::endl;
+						//std::cout << "DEBUG: " << line << std::endl;
+						s << line << std::endl;
+                                                select[i].lineNumber = lineNumber; // storing the location of start of that table
+                                                std::cout << "Table number " << tableIndex << " starts on line " << lineNumber << std::endl;
+                                                //foundTable = true;
+                                        }
+					else { // 1.b) if at the start of a table we don't want to access
+						//std::cout << "DEBUG: at the start of a table we don't want to access" << std::endl;
+						continue;
+                                	}
 				}
-			}
-			std::cout << "Parsed table " << select[i].tableIndex << std::endl;
-			out_file << s.str() << std::endl;
-			out_file.close();
-			std::cout << "Output file closed" << std::endl;
-		}
-	}
-	in_file.close();
-	std::cout << "END" << std::endl;
-}
+                                else { // 2. if inside a table
+					//std::cout << "DEBUG: inside a table" << std::endl;
+					if(lineNumber < select[i].lineNumber+76) { // 2.a) if inside a table we want to access
+                                		//std::cout << "DEBUG: inside a table we want to access" << std::endl;
+						s << line << std::endl;
+						//std::cout << "DEBUG: " << line << std::endl;
+                                	}
+					else { // 2.b) if inside a table we don't want to access
+						//std::cout << "DEBUG: inside a table we don't want to access" << std::endl;
+					}
+				}
+                        }
+                }
+                std::cout << "Finished parsing table" << std::endl;
+                std::cout << "Storing the parsed table " << select[i].tableIndex << " in ouput file " << out_filename << std::endl;
+                out_file << s.str() << std::endl;
+                out_file.close();
+                std::cout << "Output file closed" << std::endl;
+                in_file.close();
+                std::cout << "Input file closed" << std::endl;
+        }
 
+        std::cout << "Done!" << std::endl;
+}
